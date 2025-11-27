dbdd7ccb56dc6a80471388fa4961274f5b70b0d8
Angie161 <aramirez2022@udec.cl>
2025-11-26 22:34:17 -0300

    modificacion de parametros de fm_index

--- src/FMIndex.hpp
+++ src/FMIndex.hpp
@@ -3,9 +3,9 @@
 #include <sdsl/suffix_arrays.hpp>
 
 class FMIndex {
-  private:
-    sdsl::csa_wt<sdsl::wt_huff<>, 32, 32> fm_index;
-    std::vector<std::size_t> offsets;
+
+     private:
+    sdsl::csa_wt<sdsl::wt_huff<>, 32, 32> fm_index; std::vector<std::size_t> offsets;
     std::string text;
 
   public:
